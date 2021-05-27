1102fa6c70dbae218ad30cf6a7886f6e643277bb
john <john.d.leonetti@gmail.com>
2021-05-27 14:18:18 -0700

    commit tree e1fa58dd1539b1d252e63baba389d20d55249d61

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+
+const num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2;
+
+
+let number = 2
+let random = number
+
+
+
+
+let num3 = 10
+let num4 = 6 
+let mod = num3 - num4;
+
+
+let max = Math.max(5,10,15,20);
